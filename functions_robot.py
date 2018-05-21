89cd8e21075521bc2b2b5a9e9f17a0ef2a54fc3c
Belle Orlandi <isabelle.dorlandi@hotmail.com>
2018-05-21 18:30:47 -0300

    1. Funções de transformação homogênea e cinemática direta - Inicio

--- functions_robot.py
+++ functions_robot.py
@@ -2,7 +2,15 @@
 import math 
 import numpy as np
 
-def dh(theta, d, a, alpha):	
+def deg2rad(deg):
+	#
+	#Transforma angulos em graus para radianos
+	#
+	rad = deg * math.pi / 180
+	return 	rad
+	#Fim da funcao graus para radianos
+
+def transf_homogenea((theta, d, a, alpha)):	
 	#
 	# Retorna a matriz transformacao homogenea entre juntas sucessivas
 	# Os parametros de entrada estao de acordo com a convecao de Denavit Hartenberg
@@ -13,14 +21,6 @@ def dh(theta, d, a, alpha):
     			 [ 0         ,             0          ,          0             ,        1      ]])
 	return A
 	#Fim da funcao DH
-
-def deg2rad(deg):
-	#
-	#Transforma angulos em graus para radianos
-	#
-	rad = deg * math.pi / 180
-	return 	rad
-	#Fim da funcao graus para radianos
 
 
 class Robot:
@@ -34,9 +34,22 @@ class Robot:
 		self.d = np.array([0, 0, 0, 0, 0.050])
 		self.a = np.array([-0.0265, 0.1029, 0.0957, 0.0300, 0])
 		self.alpha = np.array([math.pi/2, 0, 0, -math.pi/2, 0])
+		self.tool = np.array([0,    0.1, 0, 0])
+		self.base = np.array([0, 0.0593, 0, 0])
 
+	def hello(self):
+		print 'Ola, meu nome e', self.name , '\n'
+
+#	def elo(self, n):
+#		elo = np.array([self.theta[n], self.d[n], self.a[n], self.alpha[n]])
+#		return elo
 	def elo(self, n):
-		elo = np.array([self.theta[n], self.d[n], self.a[n], self.alpha[n]])
-		return elo
+		theta = self.theta[n]
+		d = self.d[n]
+		a = self.a[n]
+		alpha = self.alpha[n]
+		return theta, d, a, alpha
+
+
 
 __version__ = '0.1'
--- main.py
+++ main.py
@@ -1,12 +1,39 @@
 # main.py
-
+import sys
 import cv2
 from functions_robot import *
 
 
-A = dh(0,90,0,1)
-print (A)
-r = Robot('5dof')
-print (r.elo(0))
-print (r.elo(1))
-print (r.elo(2))
+# -------------------------------------------
+# Programacao Visao Computacional
+#--------------------------------------------
+
+
+# Retorna (x,y,z) - coordenadas destino do elemento final do manipulador.
+
+
+# -------------------------------------------
+# Programacao Cinematica 
+#--------------------------------------------
+
+
+r = Robot('robot') 
+r.hello()
+
+r.theta = np.array([0, 0, 0, 0, 0]) #Angulos das juntas desejado para a posicao destino
+
+#Q = r.elo(0)
+#A = transf_homogenea(Q)
+#print A 
+
+
+print r.elo(0) , "\n" 
+
+T = transf_homogenea(r.base)
+print 'Base: \n', T, "\n" 
+
+T1 = transf_homogenea(r.elo(0))
+print '1: \n', T1, "\n" 
+
+
+
